859e3a2970b68496e4a1dcc25755906c7d127f11
FOSS-ter <foss-ter@localhost.localdomain>
2019-12-18 21:18:31 -0500

    Added the ship to the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,17 +1,20 @@
 import sys, pygame
-
+from settings import Settings
+from ship import Ship
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
 
     def __init__(self):
         """Initialize the game, and create game resources"""
         pygame.init()
-
-        self.screen = pygame.display.set_mode((1920,1080))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+                    (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
         # Set the background color.
-        self.bg_color = (255,0,0)
+        self.bg_color = (233,233,233)
     def run_game(self):
         """Start the main loop for the game"""
         while True:
@@ -20,7 +23,8 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill((255,0,0))
+            self.screen.fill((self.settings.bg_color))
+            self.ship.blitme()
             # Make the most recently drawn screen visible.
             # This will create the illusion of movement by constantly redrawing the screen
             pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class for storing all settings for the game"""
+    def __init__(self):
+        """Default settings"""
+        # Screen settings
+        self.screen_width = 1920
+        self.screen_height = 1080
+        self.bg_color = (230,230,230)
+
