e45d41b5a0858f9e50f3a2bfe56ec4815614b3ec
Cunningham-Wayne-Jeremy <ultima62508@gmail.com>
2019-12-30 16:31:26 -0500

    Modified size of window for testing

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ class Settings:
     def __init__(self):
         """Default settings"""
         # Screen settings
-        self.screen_width = 1920
-        self.screen_height = 1080
+        self.screen_width = 1000
+        self.screen_height = 680
         self.bg_color = (230,230,230)
 
